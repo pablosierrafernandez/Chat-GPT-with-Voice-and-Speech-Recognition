9dbf399be5af39acf5c00ad8d9e1dbda54f093f5
PABLOURV <100271163+PABLOURV@users.noreply.github.com>
2023-03-15 15:14:31 +0100

    PABLO
    ULTIMA FASE

--- ChatGPT-Alexa/chat.py
+++ ChatGPT-Alexa/chat.py
@@ -9,50 +9,51 @@ import sqlite3
 ############################## CENTINELA DE INICIO
 openai.api_key=api_key
 r=sr.Recognizer()
+close=True
+def chatear():
+    conversation=""
+    engine=pyttsx3.init()
+    engine.setProperty('voice',0)
+    engine.setProperty('rate',150)
 
-conversation=""
-engine=pyttsx3.init()
-engine.setProperty('voice',0)
-engine.setProperty('rate',150)
+    miConexion=sqlite3.connect("chat.bd")
+    miCursor=miConexion.cursor()
+    try:
+        miCursor.execute("CREATE TABLE HISTORIAL(ID INTEGER PRIMARY KEY AUTOINCREMENT, PREGUNTA TEXT, RESPUESTA TEXT, FECHA DATETIME)")
+    except:
+        pass
+    try:
+        while(close):
+            with sr.Microphone() as source:
+                r.adjust_for_ambient_noise(source,duration=2)
+                print("Escuchando...")
+                audio=r.listen(source)
+            try:
+                text=str(r.recognize_google(audio,language=language_chat))
+                
 
-miConexion=sqlite3.connect("chat.bd")
-miCursor=miConexion.cursor()
-try:
-    miCursor.execute("CREATE TABLE HISTORIAL(ID INTEGER PRIMARY KEY AUTOINCREMENT, PREGUNTA TEXT, RESPUESTA TEXT, FECHA DATETIME)")
-except:
-    pass
-try:
-    while(True):
-        with sr.Microphone() as source:
-            r.adjust_for_ambient_noise(source,duration=2)
-            print("Escuchando...")
-            audio=r.listen(source)
-        try:
-            text=str(r.recognize_google(audio,language=language_chat))
+                conversation+="\Humano: "+text+"\nAI: "
+                conversation=conversation.strip()
+                response=openai.Completion.create(engine="text-davinci-003",prompt=conversation,temperature=0.9,max_tokens=150,top_p=1,frequency_penalty=0,presence_penalty=0.6, stop=["\n","Humano","AI"])
+                
+                answer=response.choices[0].text
+                conversation+=answer
+                fecha=datetime.datetime.fromtimestamp(response.created).strftime('%H:%M:%S')
+                print(f"AI ({fecha}): {answer}\n")
+                engine.say(answer)
+                engine.runAndWait()
+                
+                miCursor.execute("INSERT INTO HISTORIAL (PREGUNTA, RESPUESTA, FECHA) VALUES(?, ?, ?)",(text,answer,datetime.datetime.fromtimestamp(response.created)))
             
-
-            conversation+="\Humano: "+text+"\nAI: "
-            conversation=conversation.strip()
-            response=openai.Completion.create(engine="text-davinci-003",prompt=conversation,temperature=0.9,max_tokens=150,top_p=1,frequency_penalty=0,presence_penalty=0.6, stop=["\n","Humano","AI"])
+            except:
+                print("No reconozco tu voz")
             
-            answer=response.choices[0].text
-            conversation+=answer
-            fecha=datetime.datetime.fromtimestamp(response.created).strftime('%H:%M:%S')
-            print(f"AI ({fecha}): {answer}\n")
-            engine.say(answer)
-            engine.runAndWait()
-            
-            miCursor.execute("INSERT INTO HISTORIAL (PREGUNTA, RESPUESTA, FECHA) VALUES(?, ?, ?)",(text,answer,datetime.datetime.fromtimestamp(response.created)))
-           
-        except:
-            print("No reconozco tu voz")
+    except KeyboardInterrupt:
+        miConexion.commit()
+        miConexion.close()
+        engine.say("Hasta pronto!")
+        engine.runAndWait()
         
-except KeyboardInterrupt:
-    miConexion.commit()
-    miConexion.close()
-    engine.say("Hasta pronto!")
-    engine.runAndWait()
-    
-    print("\nHasta prontoOOO!")
-    
-    
+        print("\nHasta prontoOOO!")
+        
+        
